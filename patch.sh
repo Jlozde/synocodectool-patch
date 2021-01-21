9ca0ff9ee9da2ca1f30d6cbbe8121860c4a48231
Jlozde <Jlozde@users.noreply.github.com>
2021-01-21 23:00:12 +0300

    Update patch.sh
    6.2.3-25426-3 support added

--- patch.sh
+++ patch.sh
@@ -7,7 +7,8 @@ bin_file="synocodectool"
 conf_file="activation.conf"
 conf_path="/usr/syno/etc/codec"
 conf_string='{"success":true,"activated_codec":["hevc_dec","ac3_dec","h264_dec","h264_enc","aac_dec","aac_enc","mpeg4part2_dec","vc1_dec","vc1_enc"],"token":"123456789987654abc"}'
-opmode="patchhelp"
+conf_path1=""
+conf_library_patched=1
 
 #arrays
 declare -A binhash_version_list=(
@@ -15,6 +16,7 @@ declare -A binhash_version_list=(
     ["ec0c3f5bbb857fa84f5d1153545d30d7b408520b"]="6.1-15047-0_6.1.1-15101-4"
     ["1473d6ad6ff6e5b8419c6b0bc41006b72fd777dd"]="6.1.2-15132-0_6.1.3-15152-8"
     ["26e42e43b393811c176dac651efc5d61e4569305"]="6.1.4-15217-0_6.2-23739-2"
+    ["1d01ee38211f21c67a4311f90315568b3fa530e6"]="6.2.1-23824-0_6.2.3-25426-2"
     ["1d01ee38211f21c67a4311f90315568b3fa530e6"]="6.2.1-23824-0_6.2.3-25426-3"
 )
 
@@ -44,9 +46,6 @@ declare -a path_list=(
     "/volume1/@appstore/MediaServer/bin/"
     "/volume2/@appstore/MediaServer/bin/"
     "/volume3/@appstore/MediaServer/bin/"
-    "/volume1/@appstore/SurveillanceStation/bin/"
-    "/volume2/@appstore/SurveillanceStation/bin/"
-    "/volume3/@appstore/SurveillanceStation/bin/"
 )
 
 declare -a versions_list=(
@@ -135,23 +134,12 @@ declare -a versions_list=(
     "6.2.2 24922-6"
     "6.2.3 25423-0"
     "6.2.3 25426-0"
+    "6.2.3 25426-1"
     "6.2.3 25426-2"
     "6.2.3 25426-3"
 )
 
 #functions
-print_usage() { 
-printf "
-SYNOPSIS
-    patch.sh [-h] [-p|-r|-l]
-DESCRIPTION
-    Patch to enable transcoding without a valid serial in DSM 6+
-        -h      Print this help message
-        -p      Patch synocodectool
-        -r      Restore from original from backup
-        -l      List supported DSM versions
-"
-}
 check_path () {
     for i in "${path_list[@]}"; do
         if [ -e "$i/$bin_file" ]; then
@@ -174,35 +162,7 @@ list_versions () {
     return 0
 }
 
-patch_menu() {
-    local options=("$@")
-    echo "Available binaries to patch/restore:"
-    local PS3="Please choose which binary you want to patch/restore:"
-    select option in "${options[@]}" "Quit"; do    
-    if [[ $REPLY = "$(( ${#options[@]}+1 ))" ]] ; then
-        echo "Goodbye"
-        exit 0
-    fi
-    bin_path="$option"
-    break
-done
-}
-
-restore_menu() {
-    local options=("$@")
-    echo "Available backups to restore:"
-    local PS3="Please choose which binary you want to restore to $bin_path:"
-    select option in "${options[@]}" "Quit"; do    
-    if [[ $REPLY = "$(( ${#options[@]}+1 ))" ]] ; then
-        echo "Goodbye"
-        exit 0
-    fi
-    backup_file="$option"
-    break
-done
-}
-
-patch_common () {
+patch () {
     source "/etc/VERSION"
     dsm_version="$productversion $buildnumber-$smallfixnumber"
     if [[ ! "$dsm_version" ]] ; then
@@ -218,138 +178,111 @@ patch_common () {
         list_versions
         exit 1
     fi
-    
-    echo "Patch for DSM Version ($dsm_version) AVAILABLE!"    
+
+    echo "Patch for DSM Version ($dsm_version) AVAILABLE!"
     check_path
-    
+
     if  ! (( ${#binpath_list[@]} )) ; then
         echo "Something went wrong. Could not find synocodectool"
         exit 1
     fi
-    
-    patch_menu "${binpath_list[@]}"
-}
 
-patch () {
-    patch_common
-    local backup_path="${bin_path%??????????????}/backup"
-    local synocodectool_hash="$(sha1sum "$bin_path" | cut -f1 -d\ )"
-    if [[ "${binhash_version_list[$synocodectool_hash]+isset}" ]] ; then
-        local backup_identifier="${synocodectool_hash:0:8}"
-        if [[ -f "$backup_path/$bin_file.$backup_identifier" ]]; then
-            backup_hash="$(sha1sum "$backup_path/$bin_file.$backup_identifier" | cut -f1 -d\ )"
-            if [[ "${binhash_version_list[$backup_hash]+isset}" ]]; then
-                echo "Restored synocodectool and valid backup detected (DSM ${binhash_version_list[$backup_hash]}) . Patching..."
-                echo -e "${binhash_patch_list[$synocodectool_hash]}" | xxd -r - "$bin_path"                
-                echo "Patched successfully"
+    for option in "${binpath_list[@]}";
+    do
+        echo $option
+        bin_path="$option"
+        local backup_path="${bin_path%??????????????}/backup"
+        conf_path1=$backup_path
+        local synocodectool_hash="$(sha1sum "$bin_path" | cut -f1 -d\ )"
+        if [[ "${binhash_version_list[$synocodectool_hash]+isset}" ]] ; then
+            local backup_identifier="${synocodectool_hash:0:8}"
+            if [[ -f "$backup_path/$bin_file.$backup_identifier" ]]; then
+                backup_hash="$(sha1sum "$backup_path/$bin_file.$backup_identifier" | cut -f1 -d\ )"
+                if [[ "${binhash_version_list[$backup_hash]+isset}" ]]; then
+                    echo "Restored synocodectool and valid backup detected (DSM ${binhash_version_list[$backup_hash]}) . Patching..."
+                    echo -e "${binhash_patch_list[$synocodectool_hash]}" | xxd -r - "$bin_path"
+                    echo "Patched successfully"
+                    echo "Creating spoofed activation.conf.."
+                    if [ ! -e "$conf_path/$conf_file" ] ; then
+                        mkdir -p $conf_path
+                        echo "$conf_string" > "$conf_path/$conf_file"
+                        echo "Spoofed activation.conf created successfully"
+                        library_patch
+                        exit 0
+                    else
+                        rm "$conf_path/$conf_file"
+                        echo "$conf_string" > "$conf_path/$conf_file"
+                        echo "Spoofed activation.conf created successfully"
+                        library_patch
+                        exit 0
+                    fi
+                else
+                    echo "Corrupted backup and original synocodectool detected. Overwriting backup..."
+                    mkdir -p "$backup_path"
+                    cp -p "$bin_path" \
+                    "$backup_path/$bin_file.$backup_identifier"
+                    exit 0
+                fi
+            else
+                echo "Detected valid synocodectool. Creating backup.."
+                mkdir -p "$backup_path"
+                cp -p "$bin_path" \
+                "$backup_path/$bin_file.$backup_identifier"
+                echo "Patching..."
+                echo -e "${binhash_patch_list[$synocodectool_hash]}" | xxd -r - "$bin_path"
+                echo "Patched"
                 echo "Creating spoofed activation.conf.."
                 if [ ! -e "$conf_path/$conf_file" ] ; then
                     mkdir -p $conf_path
                     echo "$conf_string" > "$conf_path/$conf_file"
                     echo "Spoofed activation.conf created successfully"
+                    library_patch
                     exit 0
-                    else
+                else
                     rm "$conf_path/$conf_file"
                     echo "$conf_string" > "$conf_path/$conf_file"
                     echo "Spoofed activation.conf created successfully"
+                    library_patch
                     exit 0
                 fi
-            else
-                echo "Corrupted backup and original synocodectool detected. Overwriting backup..."
-                mkdir -p "$backup_path"
-                cp -p "$bin_path" \
-                "$backup_path/$bin_file.$backup_identifier"
-                exit 0
             fi
-        else    
-            echo "Detected valid synocodectool. Creating backup.."
-            mkdir -p "$backup_path"
-            cp -p "$bin_path" \
-            "$backup_path/$bin_file.$backup_identifier"
-            echo "Patching..."
-            echo -e "${binhash_patch_list[$synocodectool_hash]}" | xxd -r - "$bin_path"            
-            echo "Patched"
-            echo "Creating spoofed activation.conf.."
-            if [ ! -e "$conf_path/$conf_file" ] ; then
-                mkdir -p $conf_path
-                echo "$conf_string" > "$conf_path/$conf_file"
-                echo "Spoofed activation.conf created successfully"
-                exit 0
-            else
-                rm "$conf_path/$conf_file"
-                echo "$conf_string" > "$conf_path/$conf_file"
-                echo "Spoofed activation.conf created successfully"
-                exit 0
-            fi
-        fi
-    elif [[ "${patchhash_binhash_list[$synocodectool_hash]+isset}" ]]; then
-        local original_hash="${patchhash_binhash_list[$synocodectool_hash]}"
-        local backup_identifier="${original_hash:0:8}"
-        if [[ -f "$backup_path/$bin_file.$backup_identifier" ]]; then
-            backup_hash="$(sha1sum "$backup_path/$bin_file.$backup_identifier" | cut -f1 -d\ )"
-            if [[ "$original_hash"="$backup_hash" ]]; then
-                echo "Valid backup and patched synocodectool detected. Skipping patch."
-                exit 0
-            else
-                echo "Patched synocodectool and corrupted backup detected. Skipping patch."
-                exit 1
-            fi
-        else
-            echo "Patched synocodectool and no backup detected. Skipping patch."
-            exit 1  
-        fi
-    else
-            echo "Corrupted synocodectool detected. Please use the -r option to try restoring it."
-            exit 1
-    fi 
-}
-
-rollback () {
-    patch_common
-    local backup_path="${bin_path%??????????????}/backup"
-    local synocodectool_hash="$(sha1sum "$bin_path" | cut -f1 -d\ )"
-    if [[ "${patchhash_binhash_list[$synocodectool_hash]+isset}" ]] ; then
-        local original_hash="${patchhash_binhash_list[$synocodectool_hash]}"
-        local backup_identifier="${original_hash:0:8}"
-        if [[ -e "$backup_path/$bin_file.$backup_identifier" ]] ; then
-            local backup_hash="$(sha1sum "$backup_path/$bin_file.$backup_identifier" | cut -f1 -d\ )"
-                if [[ "$original_hash" = "$backup_hash" ]]; then
-                    cp -p "$backup_path/$bin_file.$backup_identifier" \
-                    "$bin_path"
-                    echo "Backup restored successfully (DSM ${binhash_version_list[$backup_hash]})"
+        elif [[ "${patchhash_binhash_list[$synocodectool_hash]+isset}" ]]; then
+            local original_hash="${patchhash_binhash_list[$synocodectool_hash]}"
+            local backup_identifier="${original_hash:0:8}"
+            if [[ -f "$backup_path/$bin_file.$backup_identifier" ]]; then
+                backup_hash="$(sha1sum "$backup_path/$bin_file.$backup_identifier" | cut -f1 -d\ )"
+                if [[ "$original_hash"="$backup_hash" ]]; then
+                    echo "Valid backup and patched synocodectool detected. Skipping patch."
+                    library_patch
                     exit 0
                 else
-                    echo "No valid backup found for patched synocodectool currently in use. You can download the original file for DSM ${binhash_version_list[$original_hash]}  from https://github.com/likeadoc/synocodectool-patch/."
+                    echo "Patched synocodectool and corrupted backup detected. Skipping patch."
+                    library_patch
                     exit 1
                 fi
-        else
-            echo "No backups found for patched synocodectool currently in use. You can download the original file for DSM ${binhash_version_list[$original_hash]}  from https://github.com/likeadoc/synocodectool-patch/."
-            exit 1
-        fi
-    elif [[ "${binhash_version_list[$synocodectool_hash]+isset}" ]]; then
-        echo "Detected unpatched original synocodectool. Restoring not neccessary!"
-        exit 0
-    else
-        echo "Detected corrupted synocodectool."
-        local backup_files=( "$backup_path"/* )
-        if (( ${#backup_files[@]} )); then
-            restore_menu "${backup_files[@]}"
-            echo "Checking Hash.."
-            local backup_hash="$(sha1sum "$backup_file" | cut -f1 -d\ )"
-            if [[ "${binhash_version_list[$backup_hash]+isset}" ]]; then
-                cp -p "$backup_file" \
-                "$bin_path"
-                echo "Backup restored successfully (DSM ${binhash_version_list[$backup_hash]})"
-                exit 0
             else
-                echo "Not a valid backup. You can either try restoring another backup or download the original file for DSM $dsm_version from https://github.com/likeadoc/synocodectool-patch/."
+                echo "Patched synocodectool and no backup detected. Skipping patch."
+                library_patch
                 exit 1
             fi
         else
-            echo "No backups found. You can download the original file for DSM $dsm_version from https://github.com/likeadoc/synocodectool-patch/."
+            echo "Corrupted synocodectool detected. Please use the -r option to try restoring it."
+            library_patch
             exit 1
         fi
-    fi        
+    done
+}
+
+library_patch (){
+if [["$conf_library_patched"]]; then
+    echo "Backing up libraries..."
+    cp -p "/var/packages/SynologyMoments/target/usr/lib/libsynophoto-plugin-detection.so" "/"$conf_path1"/libsynophoto-plugin-detection_backup.so"
+    rm "/var/packages/SynologyMoments/target/usr/lib/libsynophoto-plugin-detection.so"
+    wget -P "/var/packages/SynologyMoments/target/usr/lib/"
+"https://raw.githubusercontent.com/Jlozde/xpenology-6.2.3-easyfix/master/libsynophoto-plugin-detection.so"
+    echo "backed up to:"$backup_path."/libsynophoto-plugin-detection_backup.so"
+    conf_library_patched=0
+fi
 }
 
 #main()
@@ -358,21 +291,5 @@ if (( $EUID != 0 )); then
     exit 1
 fi
 
-while getopts "prhl" flag; do
-    case "${flag}" in
-        p) opmode="patch";;
-        r) opmode="patchrollback" ;;
-        h) opmode="${opmode}" ;;
-        l) opmode="listversions" ;;
-        *) echo "Incorrect option specified in command line" ; exit 2 ;;
-    esac
-done
-
-case "${opmode}" in
-    patch) patch ;;
-    patchrollback) rollback ;;
-    patchhelp) print_usage ; exit 2 ;;
-    listversions) list_versions ;;
-    *) echo "Incorrect combination of flags. Use option -h to get help."
-       exit 2 ;;
-esac
+patch
+exit 0
